6bf1510a8f93bc8d48736233987f03326168469d
Pushkar Singh <84075300+PushkarOP@users.noreply.github.com>
2021-12-13 11:08:26 +0530

    update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-#soon
+function printTime() {
+    var d = new Date();
+    var hours = d.getHours();
+    var mins = d.getMinutes();
+    var secs = d.getSeconds();
+    document.body.innerHTML = hours+":"+mins+":"+secs;
+}
+setInterval(printTime, 1000);
